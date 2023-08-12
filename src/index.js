c8f87a69b1288eb472034369b797ac4eb09539c9
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-08-12 22:56:23 +0500

    Solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const numbers = String(Math.abs(n)).split('');
+    let res = '';
+    for (let index in numbers) {
+        res = numbers[index] + res;
+    }
+    return Number(res);
 }
